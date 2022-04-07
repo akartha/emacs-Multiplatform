ee61bf2cee815982bd7404623339aadc69138f61
Arun Kartha <6831840+akartha@users.noreply.github.com>
2022-04-06 23:33:14 -0400

    Additional custom map config
        Added ak-map (C-`) to provide a map of my custom commands
            Some of the daedreth commands, for e.g. now will be using the
        new custom map
        Also added the custom-set-variables file to the git repo

---
+++ custom-vars.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-completion-style 'emacs)
+ '(package-selected-packages
+   '(org-modern general all-the-icons rustic dired-single elcord erc-hl-nicks crux org-roam json lsp-jedi xkcd expand-region nov olivetti writeroom-mode pretty-mode modus-themes restclient lsp-ui lsp-mode company-lsp pdf-tools sudo-edit org elfeed engine-mode org-present org-easy-img-insert xml-format json-reformatter-jq json-mode go-mode org-download use-package org-pdfview dash csv-mode))
+ '(send-mail-function 'smtpmail-send-it))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "ADBO" :family "Source Code Pr")))))
