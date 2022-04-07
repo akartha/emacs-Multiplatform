e3607af3cd89e0b57c339cb565d07f44838520f3
Arun Kartha <6831840+akartha@users.noreply.github.com>
2022-04-07 14:40:01 -0400

    custom keymap changes and ox-reveal
        ox-reveal config added
        ob-go added
        custom keymap change to add shortcut to dashboard

--- custom-vars.el
+++ custom-vars.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(helm-completion-style 'emacs)
  '(package-selected-packages
-   '(org-modern general all-the-icons rustic dired-single elcord erc-hl-nicks crux org-roam json lsp-jedi xkcd expand-region nov olivetti writeroom-mode pretty-mode modus-themes restclient lsp-ui lsp-mode company-lsp pdf-tools sudo-edit org elfeed engine-mode org-present org-easy-img-insert xml-format json-reformatter-jq json-mode go-mode org-download use-package org-pdfview dash csv-mode))
+   '(ox-reveal ob-go org-modern general all-the-icons rustic dired-single elcord erc-hl-nicks crux org-roam json lsp-jedi xkcd expand-region nov olivetti writeroom-mode pretty-mode modus-themes restclient lsp-ui lsp-mode company-lsp pdf-tools sudo-edit org elfeed engine-mode org-present org-easy-img-insert xml-format json-reformatter-jq json-mode go-mode org-download use-package org-pdfview dash csv-mode))
  '(send-mail-function 'smtpmail-send-it))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
