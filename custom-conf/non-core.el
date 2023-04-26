098b03c01c566567281f5d8acccda8d897a14bdb
Arun Kartha <6831840+akartha@users.noreply.github.com>
2023-04-25 21:53:54 -0400

    Added elisp code to download a file

--- custom-conf/non-core.el
+++ custom-conf/non-core.el
@@ -249,6 +249,27 @@ With PREFIX - load the latest xkcd cartoon"
 
 (define-key ak-map "X" 'ak/reload-xkcd)
 
+(require 'url)
+
+
+;;Below is from https://stackoverflow.com/questions/4448055/download-a-file-with-emacs-lisp
+(defun ak/download-file (&optional url download-dir download-name)
+  (interactive)
+  (let ((url (or url
+                 (read-string "Enter download URL: "))))
+    (let ((download-buffer (url-retrieve-synchronously url)))
+      (save-excursion
+        (set-buffer download-buffer)
+        ;; we may have to trim the http response
+        (goto-char (point-min))
+        (re-search-forward "^$" nil 'move)
+        (forward-char)
+        (delete-region (point-min) (point))
+        (write-file (concat (or download-dir
+                                "~/downloads/")
+                            (or download-name
+                                (car (last (split-string url "/" t))))))))))
+
 ;;* Experimental features
 
 
