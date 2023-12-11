29a3301a145fb536f011d47a42e9631b5da7d395
Arun Kartha <6831840+akartha@users.noreply.github.com>
2023-12-11 16:31:46 -0500

    Added function to delete svg junk metadata
    Also, removed register-preview-delay and register-preview-function as
        they seem to be deprecated in emacs 30.1

--- custom-conf/init-org-settings.el
+++ custom-conf/init-org-settings.el
@@ -354,6 +354,11 @@
   :bind (:map ak-map
               ("<f2>" . ak/get-url-title)
               ("<f3>" . org-web-tools-insert-web-page-as-entry)
+              ;; ("<f3>" . (lambda ()
+              ;;             (interactive)
+              ;;             (org-web-tools-insert-web-page-as-entry) ;;(org-web-tools-read-url-as-org)
+              ;;             (sleep-for 3)
+              ;;             (ak/delete-svg-data-lines)))
               ("<f6>" . org-web-tools-read-url-as-org)
               ("<f7>" . org-web-tools-insert-link-for-url))
   :config 
@@ -363,7 +368,16 @@
     (interactive)
     (let* ((html (org-web-tools--get-url url))
            (title (org-web-tools--html-title html)))
-      (if title (insert title) (insert url)))))
+      (if title (insert title) (insert url))))
+
+(defun ak/delete-svg-data-lines ()
+  "Deletes lines containing SVG data from the buffer."
+ (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward "\\[\\[data:image/svg\\+xml;base64,[^]]*\\]\\]" nil t)
+      (beginning-of-line)
+      (kill-line)))))
 
 (require 'org-crypt)
 (org-crypt-use-before-save-magic)
