8f48c7ca5225eafd90733ea274f75d565664c145
Arun Kartha <6831840+akartha@users.noreply.github.com>
2024-07-17 23:09:21 -0400

    Function to report word count for current section

--- custom-conf/init-nifty-utils.el
+++ custom-conf/init-nifty-utils.el
@@ -951,5 +951,58 @@ and insert org image block for it"
 ;; Then run 
 ;; (ak/process-input-org-dir-for-roam "~/Dropbox/org-files/nytimes/")
 
+;; From https://emacs.stackexchange.com/questions/69924/count-words-under-subtree-ignoring-the-properties-drawer-and-the-subheading?newreg=292bf50260404217b2b4fd90952855a5
+;; (require 'cl-lib)
+;; (require 'org-element)
+
+(defun org-element-parse-headline (&optional granularity visible-only)
+  "Parse current headline.
+GRANULARITY and VISIBLE-ONLY are like the args of `org-element-parse-buffer'."
+  (let ((level (org-current-level)))
+    (org-element-map
+    (org-element-parse-buffer granularity visible-only)
+    'headline
+      (lambda (el)
+    (and
+     (eq (org-element-property :level el) level)
+     (<= (org-element-property :begin el) (point))
+     (<= (point) (org-element-property :end el))
+     el))
+      nil 'first-match 'no-recursion)))
+
+(cl-defun org+-count-words-of-heading (&key (worthy '(paragraph bold italic underline code footnote-reference link strike-through subscript superscript table table-row table-cell))
+                        (no-recursion nil))
+  "Count words in the section of the current heading.
+WORTHY is a list of things worthy to be counted. This list should at least include the symbols:
+paragraph, bold, italic, underline and strike-through. 
+If NO-RECURSION is non-nil don't count the words in subsections."
+  (interactive (and current-prefix-arg
+            (list :no-recursion t)))
+  (let ((word-count 0))
+    (org-element-map
+    (org-element-contents (org-element-parse-headline))
+    '(paragraph table)
+      (lambda (par)
+    (org-element-map
+        par
+        worthy
+        (lambda (el)
+          (cl-incf
+           word-count
+           (cl-loop
+        for txt in (org-element-contents el)
+        when (eq (org-element-type txt) 'plain-text)
+        sum
+        (with-temp-buffer
+          (insert txt)
+          (count-words (point-min) (point-max))))
+           ))))
+      nil nil (and no-recursion 'headline)
+      )
+      (when (called-interactively-p 'any)
+      (message "Word count in section: %d" word-count))
+    word-count))
+
+(define-key ak-map "c" '("Count words in section" . org+-count-words-of-heading))
 
 (provide 'init-nifty-utils)
