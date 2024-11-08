abfc77cbcfdd799705a2e86eaf093d85983f9156
Arun Kartha <6831840+akartha@users.noreply.github.com>
2024-11-08 13:05:08 -0500

    Attempt to have a more generic prot-window config

--- custom-conf/non-core.el
+++ custom-conf/non-core.el
@@ -251,7 +251,7 @@ Use this function via a hook."
   (when (frame-parameter nil 'prot-window-popup-frame)
     (delete-frame)))
 
-(defmacro prot-window-define-with-popup-frame (command)
+(defmacro prot-window-define-with-x-popup-frame (command)
   "Define interactive function which calls COMMAND in a new frame.
 Make the new frame have the `prot-window-popup-frame' parameter."
   `(defun ,(intern (format "prot-window-popup-%s" command)) ()
@@ -266,37 +266,34 @@ Also see `prot-window-delete-popup-frame'." command)
          ((quit error user-error)
           (delete-frame frame))))))
 
+(defmacro prot-window-define-with-nonx-popup-frame (command)
+  "Define interactive function which calls COMMAND in a new frame.
+Make the new frame have the `prot-window-popup-frame' parameter."
+  `(defun ,(intern (format "prot-window-popup-%s" command)) ()
+     ,(format "Run `%s' in a popup frame with `prot-window-popup-frame' parameter.
+Also see `prot-window-delete-popup-frame'." command)
+     (interactive)
+     (let ((frame (make-frame '((prot-window-popup-frame . t) ))))
+       (select-frame frame)
+       (switch-to-buffer " prot-window-hidden-buffer-for-popup-frame")
+       (condition-case nil
+           (call-interactively ',command)
+         ((quit error user-error)
+          (delete-frame frame))))))
+
+
 (declare-function org-capture "org-capture" (&optional goto keys))
 (defvar org-capture-after-finalize-hook)
 
-;;;###autoload (autoload 'prot-window-popup-org-capture "prot-window")
-(prot-window-define-with-popup-frame org-capture)
-
 (add-hook 'org-capture-after-finalize-hook #'prot-window-delete-popup-frame)
 
-(prot-window-define-with-popup-frame ak/clip-web-page-title-and-search-org-roam)
-;; (declare-function tmr "tmr" (time &optional description acknowledgep))
-;; (defvar tmr-timer-created-functions)
-
-;;;###autoload (autoload 'prot-window-popup-tmr "prot-window")
-;; (prot-window-define-with-popup-frame tmr)
-
-;; (add-hook 'tmr-timer-created-functions #'prot-window-delete-popup-frame)
-
-;; ;;;; The emacsclient call depends on the daemon or `server-mode' (I use the latter)
-
-;; (use-package server
-;;   :ensure nil
-;;   :defer 1
-;;   :config
-;;   (unless (server-running-p)
-;;     (server-start)))
-
-;;;; The emacsclient calls that need ot be bound to system-wide keys
-
-;; emacsclient -e '(prot-window-popup-org-capture)'
-;; emacsclient -e '(prot-window-popup-tmr)'
-
+(if ak/my-framework-p 
+    (progn 
+      (prot-window-define-with-x-popup-frame ak/clip-web-page-title-and-search-org-roam)
+      (prot-window-define-with-x-popup-frame org-capture))
+  (prot-window-define-with-nonx-popup-frame ak/clip-web-page-title-and-search-org-roam)
+  (prot-window-define-with-nonx-popup-frame org-capture))
+  
 
 (provide 'non-core)
 
